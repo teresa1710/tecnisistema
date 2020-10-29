7df79fd2e10b1881db7bd7a91e658d7829815c34
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2020-10-29 16:51:17 -0400

    Primer Commit

---
+++ js/links.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#costco').click(function (event) {
+        event.preventDefault();
+        alert("Recuerda activar la VPN para poder acceder a este link");
+    });
+
+    $('#homedepot').click(function(event){
+        event.preventDefault();
+        alert("Recuerda activar la VPN para poder acceder a este link");
+    });
+});

cbc4ab117ded43d05afd61cfba1025d566a4b465
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2020-11-14 22:12:33 -0400

    Avances JS

---
+++ js/areas-de-negocio-mostrar.js
@@ -0,0 +1,19 @@
+function mostrarComputacion(){
+    document.getElementById('computacion').style.display="block";
+}
+
+function mostrarRedes(){
+    document.getElementById('redes').style.display="block";
+}
+
+function mostrarSeguridad(){
+    document.getElementById('seguridad').style.display="block";
+}
+
+function mostrarEnergia(){
+    document.getElementById('energia').style.display="block";
+}
+
+function mostrarDesarrollo(){
+    document.getElementById('desarrollo').style.display="block";
+}

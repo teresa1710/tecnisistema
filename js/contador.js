6ea349c699a117b0f489411d7f12e89190de8c32
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2020-12-04 00:15:13 -0400

    Al fin salio los numeros

---
+++ js/contador.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function(){
+    $('.numero').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});*/
+
+jQuery(document).ready(function($) {
+    console.log("Hola");
+    $('.numero').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
